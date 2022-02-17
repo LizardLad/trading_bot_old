ec22c4bf8f5b2eed99f9d140ec56d37aad0917e7
Oliver <oliver.russell.strong@gmail.com>
2022-02-17 14:06:42 +1000

    Start getting settings written

--- gui/src/components/Settings/index.tsx
+++ gui/src/components/Settings/index.tsx
@@ -1,10 +1,16 @@
+import './settings.css';
+
 export default function Settings() {
 	return (
 		<div className='tab_content'>
 			Settings such as API key, confidence level etc go here
 			<br />
 			<br />
-			Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.
+			<p className='settings_label'>API Key Input:</p><input type='text' name='api_key_input' className='text_input'/>
+			<br />
+			<p className='settings_label'>Token Input:</p><input type='text' name='token_input' className='text_input'/>
+			<br />
+			<p className='settings_label'>Trade Confidence Input:</p><input type='text' name='trade_confidence_input' className='text_input'/>
 		</div>
 	);
 }
